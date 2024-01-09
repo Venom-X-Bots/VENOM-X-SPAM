12b34677917c680e54b05b59d0c39bbe3850f1fb
Aryan <155894970+Venom-X-Bots@users.noreply.github.com>
2024-01-09 16:50:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @𝐏𝐲𝐗𝐞𝐧")
+print("\n𝑉𝐸𝑁𝑂𝑀-𝑋-𝐵𝑂𝑇𝑆 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝\nMy Master ---> @its_Aryaan")
 
 
 async def main():
