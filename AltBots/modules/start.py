95098abd59563955455824eb5968b393cb7d7217
Aryan <155894970+Venom-X-Bots@users.noreply.github.com>
2024-01-09 16:33:40 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -5,14 +5,14 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+        Button.inline("• 𝐶𝑂𝑀𝑀𝐴𝑁𝐷𝑆 •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/Thecchub"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
+        Button.url("• 𝐶𝐻𝐴𝑁𝑁𝐸𝐿 •", "https://t.me/Thecchub"),
+        Button.url("• 𝑆𝑈𝑃𝑃𝑂𝑅𝑇 •", "https://t.me/+mBKAuzF9eP05ZTM1")
     ],
     [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/XBOTS")
+        Button.url("• 𝑅𝐸𝑃𝑂 •", "https://t.me/+mBKAuzF9eP05ZTM1")
     ]
 ]
 
@@ -33,14 +33,14 @@ async def start(event):
         AltBot = await event.client.get_me()
         bot_name = AltBot.first_name
         bot_id = AltBot.id
-        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐀xᴇɴ](https://t.me/PyXen)**\n\n"
-        TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐼'𝑀 [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ 𝐷𝐸𝑉𝐿𝑂𝑃𝐸𝑅​ : [𝑽𝑬𝑵𝑶𝑴](https://t.me/its_Aryaan)**\n\n"
+        TEXT += f"» **𝐵𝑂𝑇 𝑉𝐸𝑅𝑆𝐼𝑂𝑁 :** `M3.3`\n"
+        TEXT += f"» **𝑃𝑌𝑇𝐻𝑂𝑁 𝑉𝐸𝑅𝑆𝐼𝑂𝑁 :** `3.11.3`\n"
+        TEXT += f"» **𝑇𝐸𝐿𝐸𝑇𝐻𝑂𝑁 𝑉𝐸𝑅𝑆𝐼𝑂𝑁 :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg",
+                    "https://graph.org/file/8d771cd45a1bc53254501.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
                 )
